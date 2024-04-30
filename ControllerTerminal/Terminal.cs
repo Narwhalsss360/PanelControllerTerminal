4fbeda5c9c26898bda7b07bd05c579b02f59565f
Franklyn Vasquez <franklynvasquez24@gmail.com>
2024-04-29 23:11:28 -0400

    Start

---
+++ ControllerTerminal/Terminal.cs
@@ -0,0 +1,79 @@
+﻿using CLIApplication;
+using PanelController.Controller;
+using System.Windows.Threading;
+
+namespace ControllerTerminal
+{
+    public static class Terminal
+    {
+        public static readonly string WorkingDirectory = Environment.CurrentDirectory;
+
+        public static readonly string ExtensionsFolder = "Extensions";
+
+        public static readonly string PanelsInfoFolder = "PanelsInfo";
+
+        private static CLIInterpreter? _interpreter;
+
+        private static Dispatcher? _dispatcher;
+
+        public static CLIInterpreter Interpreter
+        {
+            get
+            {
+                if (_interpreter is null)
+                {
+                    throw new InvalidProgramException();
+                }
+
+                return _interpreter;
+            }
+        }
+
+        public static Dispatcher Dispatcher
+        {
+            get
+            {
+                if (_dispatcher is null)
+                {
+                    throw new InvalidProgramException();
+                }
+
+                return _dispatcher;
+            }
+        }
+
+        public static void LoadExtensions()
+        {
+
+        }
+
+        public static void LoadPanels()
+        {
+
+        }
+
+        public static void LoadProfiles()
+        {
+
+        }
+
+        private static void ControllerInitialized()
+        {
+            
+        }
+
+        private static void Init(CLIInterpreter interpreter, Dispatcher dispatcher)
+        {
+            _interpreter = interpreter;
+            _dispatcher = dispatcher;
+            if (Main.IsInitialized)
+            {
+                ControllerInitialized();
+            }
+            else
+            {
+                Main.Initialized += (sender, args) => ControllerInitialized();
+            }
+        }
+    }
+}
---
+++ TerminalHost/Program.cs
@@ -0,0 +1,38 @@
+﻿using CLIApplication;
+using ControllerTerminal;
+using System.Windows.Threading;
+
+namespace TerminalHost
+{
+    public static class Program
+    {
+        private static CLIInterpreter _interpreter = new();
+
+        private static Thread _interpreterThread = new(() => { _interpreter.Run(); });
+
+        private static void Initialized(object? sender, EventArgs args)
+        {
+            _interpreterThread.Start();
+            Dispatcher.Run();
+        }
+
+        private static void Deinitialized(object? sender, EventArgs args)
+        {
+            Terminal.Dispatcher.Invoke(() => { Terminal.Dispatcher.DisableProcessing(); });
+            Dispatcher.ExitAllFrames();
+            _interpreter.Stop();
+            Environment.Exit(0);
+        }
+
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            _ = (typeof(Terminal).
+                GetMethod("Init", System.Reflection.BindingFlags.NonPublic | System.Reflection.BindingFlags.Static)?.
+                Invoke(null, new object[] { _interpreter, Dispatcher.CurrentDispatcher }));
+            PanelController.Controller.Main.Initialized += Initialized;
+            PanelController.Controller.Main.Deinitialized += Deinitialized;
+            PanelController.Controller.Main.Initialize();
+        }
+    }
+}
