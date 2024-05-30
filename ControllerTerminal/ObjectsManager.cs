3caccca441db8b11f3f3a593d0c407b42fb15058
Franklyn Vasquez <franklynvasquez24@gmail.com>
2024-05-29 23:31:53 -0400

    Re-implemented ObjectManager

--- ControllerTerminal/ObjectsManager.cs
+++ ControllerTerminal/ObjectsManager.cs
@@ -12,7 +12,9 @@ namespace ControllerTerminal
 
         private static List<IPanelObject> Objects { get => s_objects ?? throw new InvalidProgramException($"Uninitialied {nameof(ObjectsManager)}"); }
 
-        private static List<Predicate<IPanelObject>> s_watchObject = new();
+        private static List<Predicate<IPanelObject>> s_toWatchPredicates = new();
+
+        private static List<Delegate> s_watchSetups = new();
 
         private static List<Delegate> s_disposers = new();
 
@@ -20,10 +22,13 @@ namespace ControllerTerminal
         {
             if (s_objects is not null)
                 return;
+
             s_objects = new();
             Extensions.Objects.CollectionChanged += ObjectsChanged;
+
             AddWatch(WatchDisposables);
             AddWatch(WatchWindows);
+            AddWatchSetup(SetupWindowWatch);
             AddDisposer(DisposeDisposable);
             AddDisposer(DisposeWindow);
         }
@@ -34,7 +39,14 @@ namespace ControllerTerminal
                 return;
 
             if (args.NewItems is not null)
-                Objects.AddRange(args.NewItems.Cast<IPanelObject>().Where(@object => Watch(@object)));
+            {
+                foreach (IPanelObject toWatch in args.NewItems.Cast<IPanelObject>().Where(@object => Watch(@object)))
+                {
+                    if (GetWatchSetupFor(toWatch) is Delegate watchSetup)
+                        watchSetup.DynamicInvoke(toWatch);
+                    Objects.Add(toWatch);
+                }
+            }
 
             foreach (IPanelObject @object in Objects.Where(@object => (args.OldItems is not null ? args.OldItems.Contains(@object) : false) || args.Action == NotifyCollectionChangedAction.Reset))
             {
@@ -49,10 +61,26 @@ namespace ControllerTerminal
                     Logger.Log($"An exception was thrown trying to dispose {@object}, {e}: {e.Message}.", Logger.Levels.Error, $"TerminalController {nameof(ObjectsManager)}");
                 }
             }
-            
         }
 
-        public static void AddWatch(Predicate<IPanelObject> watch) => s_watchObject.Add(watch);
+        public static void AddWatch(Predicate<IPanelObject> watch) => s_toWatchPredicates.Add(watch);
+
+        public static void AddWatchSetup(Delegate watchSetup)
+        {
+            MethodInfo methodInfo = watchSetup.GetMethodInfo();
+            ParameterInfo[] parameters = methodInfo.GetParameters();
+
+            if (parameters.Length != 1)
+                throw new ArgumentException($"{nameof(watchSetup)}: WatchSetup must have 1 parameter");
+
+            if (parameters[0].GetType() == typeof(IPanelObject))
+                throw new ArgumentException($"{nameof(watchSetup)}: WatchSetup parameter cannot be IPanelObject, it encapsulates all objects");
+
+            if (s_disposers.Any(existingWatchSetup => existingWatchSetup.GetMethodInfo().GetParameters()[0].ParameterType == parameters[0].ParameterType))
+                throw new ArgumentException($"{nameof(watchSetup)}: WatchSetup parameter {parameters[0].ParameterType} already exists");
+
+            s_watchSetups.Add(watchSetup);
+        }
 
         public static void AddDisposer(Delegate disposer)
         {
@@ -65,7 +93,18 @@ namespace ControllerTerminal
             if (parameters[0].GetType() == typeof(IPanelObject))
                 throw new ArgumentException($"{nameof(disposer)}: Disposer parameter cannot be IPanelObject, it encapsulates all objects");
 
+            if (s_disposers.Any(existingDisposer => existingDisposer.GetMethodInfo().GetParameters()[0].ParameterType == parameters[0].ParameterType))
+                throw new ArgumentException($"{nameof(disposer)}: Disposer parameter {parameters[0].ParameterType} already exists");
+
             s_disposers.Add(disposer);
+        }
+
+        public static Delegate? GetWatchSetupFor(IPanelObject @object)
+        {
+            foreach (Delegate watchSetup in s_watchSetups)
+                if (@object.GetType().IsAssignableTo(watchSetup.GetMethodInfo().GetParameters()[0].ParameterType))
+                    return watchSetup;
+            return null;
         }
 
         public static Delegate? GetDisposerFor(IPanelObject @object)
@@ -76,13 +115,24 @@ namespace ControllerTerminal
             return null;
         }
 
-        public static bool Watch(IPanelObject @object) => s_watchObject.Any(watch => watch(@object) && GetDisposerFor(@object) is not null);
+        public static bool Watch(IPanelObject @object) => s_toWatchPredicates.Any(watch => watch(@object) && GetDisposerFor(@object) is not null);
 
         private static bool WatchDisposables(IPanelObject @object) => @object.GetType().IsAssignableTo(typeof(IDisposable));
 
         private static void DisposeDisposable(IDisposable disposable) => disposable.Dispose();
 
         private static bool WatchWindows(IPanelObject @object) => @object.GetType().IsAssignableTo(typeof(Window));
+
+        private static void SetupWindowWatch(Window window)
+        {
+            IPanelObject @object = (IPanelObject)window;
+
+            window.Closed += (sender, args) =>
+            {
+                if (Extensions.Objects.Contains(@object))
+                    Extensions.Objects.Remove(@object);
+            };
+        }
 
         private static void DisposeWindow(Window window) => window.Dispatcher.Invoke(() => window.Close());
     }
